4c1820096dcec2d3bb51d4affba5807205590f10
piyushrawatt <piyushrawatt018@gmail.com>
2026-04-23 19:35:44 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,10 @@ app.use(cors({
 }))
 app.use(express.json())
 app.use("/routes",save)
+ 
+app.get("/",(req,res)=>{
+   res.send("backend is running")
+})
 
 mongoose.connect(process.env.MONGO_URI)
 .then(()=>{
