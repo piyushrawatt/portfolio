9e578e34febf8fede9457bf776f64b5f9adcce1c
piyushrawatt <piyushrawatt018@gmail.com>
2026-05-13 20:51:53 +0530

    updated project

--- backend/mailer.js
+++ backend/mailer.js
@@ -1,32 +1,43 @@
-import nodemailer from "nodemailer"
-import dotenv from "dotenv"
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
 const sendMail = async (to, subject, text) => {
+
   try {
+
     const transporter = nodemailer.createTransport({
-      service: "gmail",
+
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
+      family: 4,
+
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
       }
-    })
+
+    });
 
     const mailOptions = {
       from: process.env.EMAIL_USER,
       to,
       subject,
       text
-    }
+    };
 
-    await transporter.sendMail(mailOptions)
+    await transporter.sendMail(mailOptions);
 
-    console.log("Email sent successfully")
+    console.log("Email sent successfully");
+
   } catch (error) {
-    console.error("Error sending email:", error)
-    throw error   // important: don't use res here
-  }
-}
 
-export default sendMail
+    console.error("Error sending email:", error);
+
+    throw error;
+  }
+};
+
+export default sendMail;
